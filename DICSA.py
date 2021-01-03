5063806faf0327344805537465e7f79c6ccf2a2e
Disquietudee <50265869+Disquietudee@users.noreply.github.com>
2021-01-03 21:17:08 +0800

    Update DICSA.py

--- DICSA.py
+++ DICSA.py
@@ -3,11 +3,9 @@ def menu():
     print("============================================")
     print("Welcome to Electronic Services & Protection:")
     print("============================================\n\n")
-
-    print("\t1. Display Our List of Services\n\t2. Search for service\n\t3. Display added services\n\t4. Payment\n\t5. Exit ESP\n\n")
-
     while True:
-        val = 0
+        print("\t1. Display Our List of Services\n\t2. Search for service\n\t3. Display added services\n\t4. Payment\n\t5. Exit ESP\n\n")
+
         try:
             value = input("\tPlease input your choice of action (ENTER to exit):")
             val = int(value)
